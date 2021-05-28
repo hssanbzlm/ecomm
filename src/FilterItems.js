762c0c4d413c5144bcd26483ed0d89648c6456ae
Hassan Bouzlima <bouzlima.h@live.fr>
2021-05-28 14:52:53 +0100

    Display marque filter && Enhance performance using useCallback :zap:

--- src/FilterItems.js
+++ src/FilterItems.js
@@ -3,26 +3,38 @@ import "./filteritems.css";
 import Slider from "@material-ui/core/Slider";
 import Typography from "@material-ui/core/Typography";
 
-function FilterItems({ handlePriceFilter, initMinMaxPrice, initialValue }) {
+const FilterItems = React.memo(function FilterItems({
+  handlePriceFilter,
+  initMinMaxPrice,
+  value,
+  marques,
+}) {
   function handleChange(e, newValue) {
     handlePriceFilter(newValue);
   }
-
+  console.log(marques);
   return (
     <div className="filter-container">
       <Typography id="range-slider">Price</Typography>
       <Slider
-        value={initialValue}
+        value={value}
         min={initMinMaxPrice[0]}
         max={initMinMaxPrice[1]}
         step={50}
         onChange={handleChange}
-        valueLabelDisplay="auto"
+        valueLabelDisplay="on"
         aria-labelledby="range-slider"
         style={{ width: "80%", color: "gray" }}
+        marks={true}
       />
+      <Typography id="range-slider">Marques</Typography>
+      {Object.keys(marques).map((key) => (
+        <div key={key}>
+          {key} {marques[key]}{" "}
+        </div>
+      ))}
     </div>
   );
-}
+});
 
 export default FilterItems;
--- src/Item.js
+++ src/Item.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./item.css";
 import axios from "axios";
 import Paginator from "./Paginator";
 import FilterItems from "./FilterItems";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 function Item({ match }) {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(null);
   const [priceFilter, setPriceFilter] = useState([]);
-  const [loadFilter, setLoadFilter] = useState(false);
-  const [minMax, setMinMax] = useState([]);
-  function getData() {
+  const [loadFilter, setLoadFilter] = useState();
+  const [minMaxPrice, setMinMaxPrice] = useState([]);
+  const [marques, setMarques] = useState({});
+
+  const memoizedGetData = useCallback(() => {
     axios
       .get(`http://localhost:3000/${match.params.item}`)
       .then((res) => {
@@ -18,20 +21,14 @@ function Item({ match }) {
       .catch((err) => {
         console.log(err);
       });
-  }
-
-  useEffect(() => {
-    getData();
-    console.log(loadFilter);
   }, [match.params.item]);
 
   useEffect(() => {
-    if (items.length > 0) {
-      findMinMax();
-      setLoadFilter(true);
-    }
-  }, [items]);
-  function findMinMax() {
+    setLoadFilter(false);
+    memoizedGetData();
+  }, [memoizedGetData]);
+
+  const memoizedFindMinMax = useCallback(() => {
     var max = items.reduce((a, b) => {
       if (Math.max(a.price, b.price) === a.price) return a;
       else return b;
@@ -41,26 +38,53 @@ function Item({ match }) {
       if (Math.min(a.price, b.price) === a.price) return a;
       else return b;
     });
-    setMinMax([min.price, max.price]);
+    setMinMaxPrice([min.price, max.price]);
     setPriceFilter([min.price, max.price]);
-  }
+  }, [items]);
 
-  function handlePriceFilter(v) {
-    setPriceFilter(v);
-    console.log("ITEM RERENDER");
-  }
+  const memoizedFindMarques = useCallback(() => {
+    var marques = [];
+
+    for (let i = 0; i < items.length; i++) {
+      if (marques[items[i].marque]) {
+        marques[items[i].marque]++;
+      } else {
+        marques[items[i].marque] = 1;
+      }
+    }
+
+    setMarques(marques);
+  }, [items]);
+  useEffect(() => {
+    if (items) {
+      memoizedFindMinMax();
+      memoizedFindMarques();
+      setLoadFilter(true);
+    }
+  }, [memoizedFindMarques, memoizedFindMinMax, items]);
+
+  const memoizedHandlePriceFilter = useCallback(
+    (v) => {
+      setPriceFilter(v);
+    },
+    [setPriceFilter]
+  );
 
   return (
     <div className="item-container">
       {loadFilter ? (
-        <FilterItems
-          handlePriceFilter={handlePriceFilter}
-          initMinMaxPrice={minMax}
-          initialValue={priceFilter}
-        />
-      ) : undefined}
-
-      <Paginator data={items} pageSize={2} priceFilter={priceFilter} />
+        <>
+          <FilterItems
+            handlePriceFilter={memoizedHandlePriceFilter}
+            initMinMaxPrice={minMaxPrice}
+            value={priceFilter}
+            marques={marques}
+          />
+          <Paginator data={items} pageSize={4} priceFilter={priceFilter} />
+        </>
+      ) : (
+        <CircularProgress size={50} />
+      )}
     </div>
   );
 }
