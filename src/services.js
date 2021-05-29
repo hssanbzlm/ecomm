99895529bbe923a1d360aef8bf9e3eee523f0870
Hassan Bouzlima <bouzlima.h@live.fr>
2021-05-29 13:41:35 +0100

    Adding Item service :recycle:

---
+++ src/services.js
@@ -0,0 +1,35 @@
+export default function findMaxPrice(data) {
+  return data.reduce((a, b) => {
+    if (Math.max(a.price, b.price) === a.price) return a;
+    else return b;
+  });
+}
+
+export function findMinPrice(data) {
+  return data.reduce((a, b) => {
+    if (Math.min(a.price, b.price) === a.price) return a;
+    else return b;
+  });
+}
+
+export function countMarques(data) {
+  var marques = {};
+  for (let i = 0; i < data.length; i++) {
+    if (marques[data[i].marque]) {
+      marques[data[i].marque]++;
+    } else {
+      marques[data[i].marque] = 1;
+    }
+  }
+  return marques;
+}
+export function initializeFilterMarque(data) {
+  var marquesFilter = {};
+
+  for (let i = 0; i < data.length; i++) {
+    if (!marquesFilter[data[i].marque]) {
+      marquesFilter[data[i].marque] = true;
+    }
+  }
+  return marquesFilter;
+}
