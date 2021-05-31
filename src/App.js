e2846facf44f7a7ea996d98480eee1ccc8bf0ff6
Hassan Bouzlima <bouzlima.h@live.fr>
2021-05-31 12:57:51 +0100

    Add context API :hammer:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import Container from "./Container";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BasketContext } from "./Context";
 function App() {
+  const [context, setContext] = useState([]);
+
   return (
     <Router>
-      <NavBar />
-      <Container />
-      <Footer />
+      <BasketContext.Provider value={[context, setContext]}>
+        <NavBar />
+        <Container />
+        <Footer />
+      </BasketContext.Provider>
     </Router>
   );
 }
