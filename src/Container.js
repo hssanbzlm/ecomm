14dfeed87fd6990345ffe7011f69b25047c5747a
Hassan Bouzlima <bouzlima.h@live.fr>
2021-06-01 14:40:26 +0100

    Update routing :truck:

--- src/Container.js
+++ src/Container.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./container.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import DetailsBasket from "./DetailsBasket";
 import Item from "./Item";
-
+import Commande from "./Commande";
+import { BasketContext } from "./Context";
 function Container() {
+  const [basket] = useContext(BasketContext);
   return (
     <div className="main-container">
       <Switch>
         <Route exact path="/detailsbasket" component={DetailsBasket} />
+        <Route exact path="/commande">
+          {basket.length > 0 ? <Commande /> : <Redirect to="/pc" />}
+        </Route>
         <Route exact path="/:item" component={Item} />
       </Switch>
     </div>
