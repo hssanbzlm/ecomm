838fb9b4bd44ff53533592578dbd9b8d40c1ac48
Hassan Bouzlima <bouzlima.h@live.fr>
2021-06-03 17:12:53 +0100

    User commande feature :sparkles:

--- src/Commande.js
+++ src/Commande.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./commande.css";
+import UserForm from "./UserForm";
 
 function Commande() {
-  return <div className="commande-container"></div>;
+  return (
+    <div className="commande-container">
+      <div className="user-form-container">
+        <UserForm />
+      </div>
+      <div className="user-commande"></div>
+    </div>
+  );
 }
 
 export default Commande;
--- src/services.js
+++ src/services.js
@@ -39,3 +39,49 @@ export function calculSum(data) {
     return acc + current.qte * current.price;
   }, 0);
 }
+
+export function validateForm(data) {
+  let errors = {};
+  const nameRegex = /^[A-Za-z]+$/;
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  if (!data.email) {
+    errors.email = "Email is required";
+  } else if (!emailRegex.test(data.email)) {
+    errors.email = "Invalid email address";
+  }
+  if (!data.name) {
+    errors.name = "Name is required";
+  } else if (!nameRegex.test(data.name)) {
+    errors.name = "Invalid name ";
+  }
+
+  if (!data.lastName) {
+    errors.lastName = "Lastname is required";
+  } else if (!nameRegex.test(data.lastName)) {
+    errors.lastName = "Invalid lastname ";
+  }
+  if (!data.address) {
+    errors.address = "Address is required";
+  } else if (data.address.trim().length < 15) {
+    errors.address = "Invalid Adress";
+  }
+  if (!data.tel) {
+    errors.tel = "Mobile number is required";
+  } else {
+    const numString = data.tel.toString(10);
+    const firstTwoDigit = parseInt(
+      numString.split("").slice(0, 2).join(""),
+      10
+    );
+    if (
+      numString.length < 8 ||
+      numString.length > 8 ||
+      firstTwoDigit < 20 ||
+      (firstTwoDigit > 59 && firstTwoDigit < 90) ||
+      (firstTwoDigit > 29 && firstTwoDigit < 50)
+    ) {
+      errors.tel = "Invalid mobile number";
+    }
+  }
+  return errors;
+}
